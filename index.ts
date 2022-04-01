b782ff2ce4743a4789a6cced63df1d47b86a120a
Florian Klampfer <mail@qwtel.com>
2022-04-01 11:39:48 +0700

    Ship es2022

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 class WorkerLocationPolyfill implements WorkerLocation {
   #url: URL;
   constructor(href: string) { this.#url = new URL(href) }
-  get hash(): string { return '' };
-  get host(): string { return this.#url.host };
-  get hostname(): string { return this.#url.hostname };
-  get href(): string { return this.#url.href };
-  get origin(): string { return this.#url.origin };
-  get pathname(): string { return '/' };
-  get port(): string { return this.#url.port };
-  get protocol(): string { return this.#url.protocol };
-  get search(): string { return '' };
+  get hash(): string { return '' }
+  get host(): string { return this.#url.host }
+  get hostname(): string { return this.#url.hostname }
+  get href(): string { return this.#url.href }
+  get origin(): string { return this.#url.origin }
+  get pathname(): string { return '/' }
+  get port(): string { return this.#url.port }
+  get protocol(): string { return this.#url.protocol }
+  get search(): string { return '' }
   toString(): string { return this.href }
 }
 
