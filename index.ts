3b3efa3a3cb9d8f649500aa6be6afd388d0f4d4c
Florian Klampfer <mail@qwtel.com>
2022-04-01 10:44:54 +0700

    Commit

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ class WorkerLocationPolyfill implements WorkerLocation {
   get hostname(): string { return this.#url.hostname };
   get href(): string { return this.#url.href };
   get origin(): string { return this.#url.origin };
-  get pathname(): string { return '' };
+  get pathname(): string { return '/' };
   get port(): string { return this.#url.port };
   get protocol(): string { return this.#url.protocol };
   get search(): string { return '' };
@@ -26,12 +26,11 @@ function polyfillLocation(event: FetchEvent): void {
   defineProperty(event.request.url, true);
 }
 
-const getProcessEnv = (k: string) => Reflect.get(Reflect.get(Reflect.get(self, 'process') || {}, 'env') || {}, k);
-
 const WORKER_LOCATION = 'WORKER_LOCATION';
 if (!('location' in self)) {
-  if (Reflect.has(self, WORKER_LOCATION) || getProcessEnv(WORKER_LOCATION) != null) {
-    defineProperty(Reflect.get(self, WORKER_LOCATION) || getProcessEnv(WORKER_LOCATION));
+  const envLoc = (<any>self)[WORKER_LOCATION] || (<any>self).process?.env?.[WORKER_LOCATION] != null;
+  if (envLoc) {
+    defineProperty(envLoc);
   } else {
     self.addEventListener('fetch', polyfillLocation);
   }
